16b03881668381801843cc4caf90fa75107e0a58
Bernardo6526 <bernardo6526@gmail.com>
2021-05-18 13:35:28 -0300

    Added interface

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,4 +1,31 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
-    alert("hello boy");
-  }
+
+let dados = [];
+
+onload = () => {
+    const dadosCarregados = JSON.parse(localStorage.getItem('dados'));
+    if (dadosCarregados) dados = dadosCarregados;
+
+    // cria um objeto com as abas
+    let tabs = document.querySelectorAll('.navBar .tab');
+
+    const mostra = (elem) => {
+        if (elem) {
+        for (let i = 0; i < tabs.length; i++) tabs[i].classList.remove('active');
+        elem.classList.add('active');
+        }
+
+        for (let i = 0; i < tabs.length; i++) {
+        let comp = tabs[i].getAttribute('for');
+        if (tabs[i].classList.contains('active'))
+            document.querySelector('#' + comp).classList.remove('hidden');
+        else document.querySelector('#' + comp).classList.add('hidden');
+        }
+    };
+
+    for (let i = 0; i < tabs.length; i++)
+        tabs[i].onclick = (e) => {
+        mostra(e.target);
+    };
+
+    mostra();
+};
